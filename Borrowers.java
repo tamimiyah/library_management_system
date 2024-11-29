2acb7d515160fd8600abc9e04abab4441131f932
tamimiyah <160693673+tamimiyah@users.noreply.github.com>
2024-11-30 06:26:21 +0800

    Create Borrowers.java

---
+++ Borrowers.java
@@ -0,0 +1,129 @@
+public class Borrowers{
+    
+    public int id;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private String gender;
+    private String birthday;
+    private String contactNumber;
+    private String email;
+    private String address;
+    private int violationNum;
+    private Material borrowedMaterial;
+
+    // Constructor
+    public Borrowers(int id, String lastName, String firstName, String middleName, String gender, String birthday, String contactNumber, String email, String address) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setEmail(String email) {    
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setViolationNum(int violationNum) {
+        this.violationNum = violationNum;
+    }
+
+    public int getViolationNum() {
+        return violationNum;
+    }
+
+    public void incrementViolationNum() {
+        violationNum++;
+    }
+
+    public Material getBorrowedMaterial() {
+        return borrowedMaterial;
+    }
+
+    public void borrowMaterial(Material material) {
+        this.borrowedMaterial = material;
+    }
+
+    public void returnMaterial() {
+        this.borrowedMaterial = null;
+    }
+
+    // Check if borrower can borrow a material
+    public boolean canBorrow(){
+        return violationNum < 3 && borrowedMaterial == null;
+    }
+
+}
