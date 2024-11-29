f7048cc001ef96e25d708ffbaf2017c8e7d9227c
tamimiyah <160693673+tamimiyah@users.noreply.github.com>
2024-11-30 06:25:42 +0800

    Create ThesisBook.java

---
+++ ThesisBook.java
@@ -0,0 +1,43 @@
+
+public class ThesisBook extends Material {
+    private String title, author;
+
+    public ThesisBook(String materialID, String title, String author, String yearPublished, String publisher, int copies) {
+        super( materialID,  yearPublished,  publisher,  copies);
+        this.title = title;
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Material ID: " + getMaterialID() +
+                "\nType: Thesis Book" +
+                "\nTitle: " + title +
+                "\nAuthor: " + author +
+                "\nYear Published: " + getYearPublished() +
+                "\nPublisher: " + getPublisher() +
+                "\nCopies: " + getCopies();
+    }
+
+    @Override
+    public int getReturnDateDays() {
+        return 2; // 2 days for thesis books
+    }
+}
+
